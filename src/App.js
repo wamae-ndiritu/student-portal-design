ab7f444ce43841b6682bffcccfd4d1756dcc77cf
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-04 00:29:20 +0300

    Set up tailwid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4 className='text-green-500'>Hello friend</h4>
     </div>
   );
 }
