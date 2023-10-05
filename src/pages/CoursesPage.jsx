6c0e696b45bf22f910049eb8a793545b02f554e8
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-05 13:54:54 +0300

    Make listTable reusable

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -2,7 +2,53 @@ import React from "react";
 import Hero from "../components/Hero";
 import TopHeader from "../components/TopHeader";
 import Stats from "../components/Stats";
-import StudentsTable from "../components/tables/StudentsTable";
+import ListTable from "../components/tables/ListTable";
+
+const columns = [
+  {
+    field: "id",
+    headerName: "Student No",
+    width: 250,
+  },
+  { field: "student_name", headerName: "Student Name", width: 250 },
+  {
+    field: "grade",
+    headerName: "Current Grade",
+    width: 150,
+  },
+  {
+    field: "attendance",
+    headerName: "Average Attedance",
+    width: 160,
+  },
+  {
+    field: "action",
+    headerName: "Action",
+    width: 100,
+    renderCell: () => {
+      return (
+        <button className='bg-red-500 rounded px-3 py-2 text-white cursor-pointer'>
+          Delete
+        </button>
+      );
+    },
+  },
+];
+
+const rows = [
+  {
+    id: "08210304058",
+    student_name: "Water John Snow",
+    grade: "80%",
+    attendance: "75%",
+  },
+  {
+    id: "0821004058",
+    student_name: "John Doe",
+    grade: "50%",
+    attendance: "65%",
+  },
+];
 
 const CoursesPage = () => {
   const title = "Hello, Anne";
@@ -19,7 +65,7 @@ const CoursesPage = () => {
         attendance={"90%"}
         coursesEnrolled={3}
       />
-      <StudentsTable />
+      <ListTable headers={columns} data={rows} title='List of Table' />
     </div>
   );
 };
